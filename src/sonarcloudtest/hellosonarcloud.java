1a98ca9202e27088d63bd7c26715ebadc7085c05
praysc <https://github.com/praysc/testSonarCloud>
2020-01-07 22:02:38 +0100

    Create hellosonarcloud.java

---
+++ src/sonarcloudtest/hellosonarcloud.java
@@ -0,0 +1,10 @@
+package sonarcloudtest;
+
+public class hellosonarcloud {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello sonar cloud");
+	}
+
+}
